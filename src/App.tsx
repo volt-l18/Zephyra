7e40b3ffa08eaaa8cd71289b58648098673dd97d
volt-l18 <ng05031975@gmail.com>
2025-02-09 21:54:46 +0530

    added logo and favicon

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ function App() {
   return (
     <>
       <div>
+        <img src="/images/logo.svg" height={500} width={500} alt="logo" />
+        <p> i asked claude to make logo for this :) </p>
         <h1> ello :) </h1>
       </div>
     </>
